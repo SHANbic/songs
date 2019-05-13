34095ce8628ce86bb9d7420e1d4b60a4d12619b6
Pierre Lambert <supagoof924@hotmail.com>
2019-05-14 01:26:36 +0200

    [ FEATURE ] Updated SongDetail Component to display datas

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const SongDetail = props => {
-  console.log(props);
-  return <div>SongDetail</div>;
+const SongDetail = ({ song }) => {
+  if (song) return (
+    <div>
+      <h3>Details for</h3>
+      <p>
+        title : {song.title}
+        <br />
+        duration : {song.duration}
+      </p>
+    </div>
+  );
+  else return <div>Select a song</div>
 };
 
 const mapStateToProps = state => {
